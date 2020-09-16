4616a365bc8b4ed276cd41c6c5a084447ba181a3
dwayneellis <70253282+dwayneellis@users.noreply.github.com>
2020-09-16 11:09:48 -0500

    Add files via upload
    Lab1.12

---
+++ lab1.12.py
@@ -0,0 +1,12 @@
+# CIS 2348 Homework 1 Fall 2020.
+# Dwayne Ellis
+# Student ID: 0833810
+# Lab 1.12
+
+userNum = int(input("Enter user number:"))
+userNumSquared = userNum * userNum
+# Bug here; fix it when instructed
+
+print(userNumSquared, end=' ')
+
+# Output formatting issue here; fix it when instructed
