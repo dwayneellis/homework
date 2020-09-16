099ef941acf1975e8b656a7ca8423dee07b44164
dwayneellis <70253282+dwayneellis@users.noreply.github.com>
2020-09-16 11:45:45 -0500

    Add files via upload

---
+++ lab2.19.py
@@ -0,0 +1,27 @@
+# CIS 2348 Homework 1 Fall 2020.
+# Dwayne Ellis
+# Student ID: 0833810
+# Lab 2.19
+
+# Prompt user for number of cups of lemon juice, water, and agave nectar needed to make lemonade
+print("Enter amount of lemon juice (in cups):")
+cups_lemon_juice = float(input('Lemon Juice:'))
+cups_water = float(input('Water:'))
+cups_agave = float(input('Agave Nectar:'))
+serving_quantity = float(input('Serving Size:'))
+
+# Lemonade ingredients
+print("Lemonade ingredients - yields", serving_quantity, "servings")
+print(cups_lemon_juice, "cup(s) lemon juice")
+print(cups_water, " cup(s) Water")
+print(cups_agave, "cup(s) agave nectar")
+
+# Prompt user to specify desired number of servings
+print("How many servings would you like to make?")
+serving_quantity = float(input('Serving Size:'))
+
+# Lemonade ingredients
+print("Lemonade ingredients - yields", serving_quantity, "servings")
+print(cups_lemon_juice, "cup(s) lemon juice")
+print(cups_water, " cup(s) Water")
+print(cups_agave, "cup(s) agave nectar")
