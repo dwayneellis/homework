eabb5a8c723158e4516167681a1eadf3c5af422a
dwayneellis <70253282+dwayneellis@users.noreply.github.com>
2020-09-16 10:16:49 -0500

    Add files via upload
    CIS 2348 Homework 1

---
+++ homework1.py
@@ -0,0 +1,44 @@
+# CIS 2348 Homework 1 Fall 2020.
+# Dwayne Ellis
+# Student ID: 0833810
+# Birthday Calculator
+# Coding Problem 1 - Calculating the age of a person based on current date and the birthday of the user.
+
+
+# Prompt user for name
+name = input("Enter your name:")
+
+# Prompt user for current date
+print("Enter current date as (MM/DD/YYYY):")
+current_month = int(input('Month:'))
+current_day = int(input('Day:'))
+current_year = int(input('Year:'))
+
+# Prompt user for their birthday
+print("Enter your birthday as (MM/DD/YYYY):")
+birthday_month = int(input('Month:'))
+birthday_day = int(input('Day:'))
+birthday_year = int(input('Year:'))
+
+# Calculate age
+
+age = current_year - birthday_year
+
+# Print entered information
+
+print("Birthday Calculator")
+print("Current Date:", current_month, current_day, current_year)
+print("Birthday:", birthday_month, birthday_day, birthday_month)
+print("Hello", name + "!", "You are", age, "years old."+'\n')
+
+
+# def print_hi(name):
+# Use a breakpoint in the code line below to debug your script.
+#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+# print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
