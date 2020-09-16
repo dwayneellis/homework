719bd850f155505b112a814147530016274b8b38
dwayneellis <70253282+dwayneellis@users.noreply.github.com>
2020-09-16 11:57:25 -0500

    Add files via upload

---
+++ lab1.20.py
@@ -0,0 +1,26 @@
+# CIS 2348 Homework 1 Fall 2020.
+# Dwayne Ellis
+# Student ID: 0833810
+# Lab 1.20
+
+# First user input user_num
+user_num = int(input("Enter user number:"))
+userNumSquared = user_num * user_num
+print("userNum squared:", userNumSquared)
+
+# Second user input user_num1
+user_num1 = int(input("Enter user_num1:"))
+print("userNum1:", user_num1)
+userNum1Squared = user_num1 * user_num1
+print("userNum1 squared:", userNum1Squared)
+userNum1Cubed = user_num1**3
+print("userNum1 cubed:", userNum1Cubed)
+
+userNum2 = int(input("Enter user_num2:"))
+Sum = userNum2
+print(Sum)
+
+userNum2Product = userNum2 * ()
+print("userNum2 Product is:", userNum2Product)
+
+# Third user input user_num2 - sum two numbers and multiply two numbers
